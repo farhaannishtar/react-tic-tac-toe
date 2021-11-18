a725c6ab3748ada0ef804582bae4ea92d260dc09
Alisha Sojar <alisha@amino.com>
2021-11-17 20:34:03 -0500

    Add scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Board from "./components/Board";
+import {calculateWinner} from "./components/helpers";
 
 import "./App.css";
 
 function App() {
-  const [boardValues, setBoardValues] = React.useState(["", "", "", "", "", "", "", "", ""]);
+  const [boardValues, setBoardValues] = React.useState(["X", "O", "X", "", "O", "O", "", "", ""]);
+  const [xIsNext, setXIsNext] = React.useState(true);
+  
+  const winner = calculateWinner(boardValues)
+
   return (
     <div className="App">
       <Board squares={boardValues} />
--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -2,7 +2,7 @@ export function addStuff(x: number, y: number) {
   return x + y;
 }
 
-export function calculateWinner() {
+export function calculateWinner(squares: string[]) {
   // TODO
   return "TODO"
 }
