4f17b032be8c04cfc4ac19fbb7367de76bd90054
Alisha Sojar <alisha@amino.com>
2021-11-17 21:07:41 -0500

    Moar scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,22 @@ import { calculateWinner } from "./components/helpers";
 import "./App.css";
 
 function App() {
+  // This represents the 9 squares of the tic-tac-toe board. 
   const [boardValues, setBoardValues] = React.useState([
     "X",
     "O",
     "X",
-    "",
+    "-",
     "O",
     "O",
-    "",
-    "",
-    "",
+    "-",
+    "-",
+    "-",
   ]);
+  // This is a boolean to help us keep track of who's turn it is. 
   const [xIsNext, setXIsNext] = React.useState(true);
+
+  // The winner.
   const winner = calculateWinner(boardValues);
 
   return (
@@ -24,7 +28,7 @@ function App() {
       <div className="App">
         <Board squares={boardValues} />
         <div>
-          Next player: {xIsNext ? "X" : "O"}
+          xIsNext current value:  {xIsNext ? 'true' : 'false' }
           <div>
             <button onClick={() => setXIsNext(false)}>
               Click me to change player
