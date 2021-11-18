a13731f79e1e81c999cb1e845dede0cac700508a
Alisha Sojar <alisha@amino.com>
2021-11-17 20:48:00 -0500

    more scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import Board from "./components/Board";
-import {calculateWinner} from "./components/helpers";
+import { calculateWinner } from "./components/helpers";
 
 import "./App.css";
 
 function App() {
-  const [boardValues, setBoardValues] = React.useState(["X", "O", "X", "", "O", "O", "", "", ""]);
+  const [boardValues, setBoardValues] = React.useState([
+    "X",
+    "O",
+    "X",
+    "",
+    "O",
+    "O",
+    "",
+    "",
+    "",
+  ]);
   const [xIsNext, setXIsNext] = React.useState(true);
-  
-  const winner = calculateWinner(boardValues)
+  const winner = calculateWinner(boardValues);
 
   return (
-    <div className="App">
-      <Board squares={boardValues} />
-    </div>
+    <>
+      <div className="App">
+        <Board squares={boardValues} />
+        <div>
+          Next player: {xIsNext ? "X" : "O"}
+          <div>
+            <button onClick={() => setXIsNext(false)}>
+              Click me to change player
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
