8b6e43bb3cf57431693f84580fc7a7d99bb794f2
Alisha Sojar <alisha@amino.com>
2021-11-18 12:04:59 -0500

    Fix Alisha's merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Board from "./components/Board";
-import {calculateWinner} from "./components/helpers";
+import { calculateWinner } from "./components/helpers";
 
 import "./App.css";
 
 function App() {
+  // This represents the 9 squares of the tic-tac-toe board.
   const [boardValues, setBoardValues] = React.useState(["X", "O", "X", "-", "O", "O", "-", "-", "-"]);
+  // This is a boolean to help us keep track of who's turn it is.
   const [xIsNext, setXIsNext] = React.useState(true);
 
   // The winner.
