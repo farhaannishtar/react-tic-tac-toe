931dd5d1356b83b804a1b50414a1c42b308e5df8
Farhaan Nishtar <89179469+farhaannishtar@users.noreply.github.com>
2021-11-18 13:59:07 +0530

    Update Square.tsx
    please have a look dear

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -5,6 +5,3 @@ type SquareProps = { value: string | number };
 export default function Square(props: SquareProps) {
   return <button className="square">{props.value}</button>;
 }
-// export default function Square(props: SquareProps) {
-//   return <button className="square">props.square</button>;
-// }
