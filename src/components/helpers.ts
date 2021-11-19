0c96343986ea8a8bd8ce913c8c005adf3b191e67
Alisha Sojar <alisha@amino.com>
2021-11-18 22:34:20 -0500

    Add more scaffolding for calculate winner

--- src/components/helpers.ts
+++ src/components/helpers.ts
@@ -1,8 +1,19 @@
-export function addStuff(x: number, y: number) {
+// This won't be used in tic tac toe.. just an example function!
+export function addStuff(x: number, y: number): number {
   return x + y;
 }
 
-export function calculateWinner(squares: string[]) {
+/*
+Given an array of 9 squares, this function will check for a winner and return 'X', 'O', or null as appropriate.
+
+If 'X' wins, return 'X'.
+If 'O' wins, should return 'O'
+If no one wins, should return null
+
+Example input: ['X', 'X', 'X', null, null, null, null, null, null]
+In the example above, 'X' is the winner since there is 3 in a row.
+*/
+export function calculateWinner(squares: string[]) : string | null {
   // TODO
-  return "TODO"
+  return null
 }
--- src/tests/helpers.test.ts
+++ src/tests/helpers.test.ts
@@ -1,6 +1,43 @@
-import { addStuff } from "../components/helpers";
+import { addStuff, calculateWinner } from "../components/helpers";
 
 test("addStuff", () => {
   expect(addStuff(1, 1)).toBe(2);
   expect(addStuff(2, 4)).toBe(6);
+});
+
+test("calculateWinner, horizontal case", () => {
+  expect(calculateWinner(["X", "X", "X", "O", "-", "O", "-", "-", "-"])).toBe(
+    "X"
+  );
+  expect(calculateWinner(["X", "X", "O", "O", "O", "O", "-", "-", "-"])).toBe(
+    "O"
+  );
+});
+
+test("calculateWinner, vertical case", () => {
+  // X should win
+  expect(calculateWinner(["X", "O", "X", "X", "-", "O", "X", "-", "-"])).toBe(
+    "X"
+  );
+  // O should win
+  expect(calculateWinner(["X", "O", "X", "X", "-", "O", "X", "-", "-"])).toBe(
+    "X"
+  );
+});
+
+test("calculateWinner, diagonal case", () => {
+  // X should win
+  expect(calculateWinner(["X", "O", "-", "O", "X", "O", "X", "-", "X"])).toBe(
+    "X"
+  );
+  // O should win
+  expect(calculateWinner(["O", "-", "-", "X", "O", "O", "-", "X", "O"])).toBe(
+    "O"
+  );
+});
+
+test("calculateWinner, no winner", () => {
+  expect(calculateWinner(["X", "O", "X", "X", "-", "O", "-", "-", "-"])).toBe(
+    null
+  );
 });
